abf820a7f0f208321597917fab0bac56b8b1e5aa
Eduardo Lemus <eduardolemus3098@gmail.com>
2025-03-16 20:03:58 -0600

    se inicializa el porycto con lo paquetes de spring boot, template engine, para completacion de codigo de archivos de configura, spring data jpa, se opta por gestor de paquete graddle

---
+++ src/main/java/com/udb/bancobas/BancoBasApplication.java
@@ -0,0 +1,13 @@
+package com.udb.bancobas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoBasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancoBasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udb/bancobas/BancoBasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udb.bancobas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoBasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
